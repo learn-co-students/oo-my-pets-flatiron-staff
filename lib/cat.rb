5d3b1e32b7fdfd9be3292d1e66f18bb0087bacfa
Emily Jennings <emily.jennings@flatironschool.com>
2018-07-31 11:44:33 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  attr_writer
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,14 @@
 class Owner
-  # code goes here
+  attr_accessor :cat, :dog, :fish
+  @@all = []
+
+  def initialize(owner)
+    @owner = owner
+  end
+
+  def self.all
+    @@all << self
+    @@all
+  end
+
 end
