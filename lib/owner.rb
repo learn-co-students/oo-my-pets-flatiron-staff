d72f558c3fa4fa161a8929cafb8db74d121b5ec2
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-01 01:50:05 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Owner
-  attr_accessor :cat, :dog, :fish, :pets, :all, :name
+  attr_accessor :cat, :dog, :fish, :pets, :all, :name, :mood
   attr_reader
   @@all = []
 
@@ -55,7 +55,30 @@ class Owner
   end
 
   def walk_dogs
-    @pets[:dogs].mood = "happy"
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    self.pets.each {|type, pets| pets.clear}
+    #binding.pry
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
 
