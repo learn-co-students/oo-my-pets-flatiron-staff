689d27fc0d0fff4c39cc2c2b5099b7bca61fae5a
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-01 00:41:05 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -40,6 +40,22 @@ class Owner
   end
 
   def buy_fish(name)
+    @fish = Fish.new(name)
+    @pets[:fishes] << @fish
+  end
+
+  def buy_cat(name)
+    @cat = Cat.new(name)
+    @pets[:cats] << @cat
+  end
+
+  def buy_dog(name)
+    @dog = Dog.new(name)
+    @pets[:dogs] << @dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].mood = "happy"
   end
 
 
