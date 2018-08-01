6d9be6ff81c489b46c62a1f7163d215d5fc48d4d
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-01 00:24:50 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,46 @@
+require 'pry'
+
 class Owner
-  attr_accessor :cat, :dog, :fish
+  attr_accessor :cat, :dog, :fish, :pets, :all, :name
+  attr_reader
   @@all = []
 
   def initialize(owner)
     @owner = owner
+    @pets = {
+      cats: [],
+      dogs: [],
+      fishes: []
+    }
+    @@all << self
   end
 
   def self.all
-    @@all << self
-    @@all
+    return @@all
   end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def species
+    "human"
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def name
+    self.name = @name
+  end
+
+  def buy_fish(name)
+  end
+
 
 end
