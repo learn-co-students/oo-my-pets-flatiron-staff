a069c622e536a2056990b862bae36211d34153d0
Anna Lux <anna.watt@flatironschool.com>
2020-05-13 12:35:24 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,33 @@
 class Fish
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |fish| fish.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.new(name)
+  end
+
 end
